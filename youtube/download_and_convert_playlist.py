f08cc4949e53539dc1ff65c0cd4fe277dc948b51
Vitor Diniz <dinizadroit@gmail.com>
2024-11-01 19:02:07 -0300

    update youtube playlist script

--- youtube/download_and_convert_playlist.py
+++ youtube/download_and_convert_playlist.py
@@ -1,58 +1,37 @@
-
 import os
-import ssl
-from pytube import Playlist
-from moviepy.editor import VideoFileClip
+import yt_dlp
 
-# Disable SSL certificate verification (use with caution)
-ssl._create_default_https_context = ssl._create_unverified_context
+def download_audio_as_mp3(url, output_folder, prefix):
+  ffmpeg_path = '/opt/homebrew/bin/ffmpeg'
 
-def convert_video_to_mp3(video_path, output_folder):
-  try:
-    # Load the video file
-    video_clip = VideoFileClip(video_path)
+  ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': f'{output_folder}/{prefix}%(title)s.%(ext)s',
+    'postprocessors': [{
+      'key': 'FFmpegExtractAudio',
+      'preferredcodec': 'mp3',
+      'preferredquality': '192',
+    }],
+    'ffmpeg_location': ffmpeg_path,
+    'progress_hooks': [progress_hook],
+    'no_warnings': True,
+    'ignoreerrors': True
+  }
 
-    # Extract only the audio
-    audio_clip = video_clip.audio
+  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
 
-    # Save the audio in MP3 format
-    mp3_path = os.path.join(output_folder, f"{os.path.splitext(os.path.basename(video_path))[0]}.mp3")
-    audio_clip.write_audiofile(mp3_path)
+def progress_hook(d):
+  if d['status'] == 'downloading':
+    print(f"Baixando: {d['filename']} - {d['_percent_str']} completo")
+  elif d['status'] == 'finished':
+    print(f"Download concluído, convertendo para MP3: {d['filename']}")
 
-    print(f'Conversion to MP3 successful: {mp3_path}')
-  except Exception as e:
-    print(f'Error in MP3 conversion: {str(e)}')
+url = input("Digite a URL do vídeo ou playlist: ")
+prefix = input("Digite o prefixo para o nome do arquivo (opcional, pressione Enter para deixar vazio): ")
+output_folder = f'{os.getcwd()}/downloads'
 
-def download_and_convert_playlist(url, output_folder='', title_prefix=''):
-  playlist = Playlist(url)
+if not os.path.exists(output_folder):
+  os.makedirs(output_folder)
 
-  for video in playlist.videos:
-    try:
-      print(f'Downloading: {title_prefix}{video.title}')
-      video_stream = video.streams.filter(file_extension="mp4", progressive=True).first()
-
-      # Extract the original file extension from the stream
-      file_extension = video_stream.mime_type.split('/')[-1]
-
-      # Download with the correct file extension
-      video_path = os.path.join(output_folder, f"{title_prefix}{video.title}.{file_extension}")
-      video_stream.download(output_path=output_folder, filename=f"{title_prefix}{video.title}.{file_extension}")
-
-      # Convert video to MP3
-      convert_video_to_mp3(video_path, output_folder)
-
-      # Delete the original video file
-      os.remove(video_path)
-      print(f'{video_path} deleted successfully!')
-
-    except Exception as e:
-      print(f'Error processing {title_prefix}{video.title}: {str(e)}')
-
-if __name__ == "__main__":
-  playlist_id = input("Enter the Playlist ID: ")
-  title_prefix = input("Enter the Title Prefix (optional): ")
-  output_folder = input("Enter the Output Folder (optional): ")
-
-  playlist_url = f"https://www.youtube.com/playlist?list={playlist_id}"
-
-  download_and_convert_playlist(playlist_url, output_folder, title_prefix)
+download_audio_as_mp3(url, output_folder, prefix)
